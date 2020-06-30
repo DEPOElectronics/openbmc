d1811736ef92390aecb6b1c11c55d1a87ceab437
makise-homura <akemi_homura@kurisa.ch>
2020-06-30 22:50:45 +0300

    Fix initsc-lt for current version of OpenBMC
    Change-Id: Iae709aa337fca25a2bcd586e73bdcec719b0ac25

--- meta-mcst/recipes-mcst/initsc-lt/files/initsc.sh
+++ meta-mcst/recipes-mcst/initsc-lt/files/initsc.sh
@@ -26,25 +26,3 @@ devmem 0x1e6e2088 w 0x01C00000
 # Set SPI master CS1# to correct mode (SCU80[15]=1, SCU90[31]=1)
 devmem 0x1e6e2090 w 0x8fffa004
 devmem 0x1e6e2080 w 0xffc08002
-
-# Set correct options file for root
-[ -f /mnt/data/etc/bashrc ] && ln -fs /mnt/data/etc/bashrc /home/root/.bashrc || touch /home/root/.bashrc
-ln -fs /home/root/.bashrc /home/root/.profile
-ln -fs /home/root/.bashrc /home/root/.bash_profile
-
-# Set correct home directory for root
-rm -rf /root
-ln -s /home/root /root
-
-# Configure lo and eth* interfaces
-ip link set dev lo up
-/usr/share/netsetup/netconf_script.sh
-echo "netconf_script.sh done"
-
-# Copy root's authorized_keys file
-mkdir -p /home/root/.ssh
-if [ -f /mnt/data/etc/authorized_keys ]
-then
-    cp /mnt/data/etc/authorized_keys /home/root/.ssh
-    chmod 600 /home/root/.ssh/authorized_keys
-fi
