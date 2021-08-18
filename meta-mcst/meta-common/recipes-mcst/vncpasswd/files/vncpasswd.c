4457d39a00ab5c93a8a985ac632017cc27b4ae96
makise-homura <akemi_homura@kurisa.ch>
2021-08-18 22:12:06 +0300

    Rewrite vncpasswd to libreimu
    Change-Id: Icb558ac3cd15bebdf1d294f109ef3dc5628438be

--- meta-mcst/meta-common/recipes-mcst/vncpasswd/files/vncpasswd.c
+++ meta-mcst/meta-common/recipes-mcst/vncpasswd/files/vncpasswd.c
@@ -4,28 +4,18 @@
 #include <string.h>
 #include <pwd.h>
 #include <rfb/rfbproto.h>
-
-char *fname = NULL;
-
-static __attribute__((format(printf, 2, 3))) int error(int code, const char *format, ...)
-{
-    va_list args;
-    va_start (args, format);
-    vfprintf (stderr, format, args);
-    va_end (args);
-    if (fname) free(fname);
-    exit(code);
-}
+#include <reimu.h>
 
 int main(int argc, char *argv[])
 {
+    char *fname = NULL;
     int pass_max = sysconf(_SC_PASS_MAX);
     if (pass_max < 0) pass_max = 8192;
 
     if (argc > 1)
     {
         fname = malloc(strlen(argv[1]) + 1);
-        if (fname == NULL) error(4, "Out of memory for destination file name\n");
+        if (fname == NULL) reimu_cancel(4, "Out of memory for destination file name\n");
         strcpy(fname, argv[1]);
     }
     else
@@ -34,29 +24,29 @@ int main(int argc, char *argv[])
         if ((homedir = getenv("HOME")) == NULL)
         {
             homedir = getpwuid(getuid())->pw_dir;
-            if (strlen(homedir) < 1) error(3, "Can't determine home directory\n");
+            if (strlen(homedir) < 1) reimu_cancel(3, "Can't determine home directory\n");
         }
 
         const char *default_fname = "/.vnc/passwd";
         fname = malloc(strlen(homedir) + strlen(default_fname) + 1);
-        if (fname == NULL) error(4, "Out of memory for destination file name\n");
+        if (fname == NULL) reimu_cancel(4, "Out of memory for destination file name\n");
         strcpy(fname, homedir);
         strcat(fname, default_fname);
     }
 
     char pass1[pass_max + 1], pass2[pass_max + 1],  *pass;
     pass = getpass("Password: ");
-    if (!pass) error(1, "Can't read password\n");
+    if (!pass) {free(fname); reimu_cancel(1, "Can't read password\n");}
     strncpy(pass1, pass, pass_max);
 
     pass = getpass("Verify: ");
-    if (!pass) error(1, "Can't read password verification\n");
+    if (!pass) {free(fname); reimu_cancel(1, "Can't read password verification\n");}
     strncpy(pass2, pass, pass_max);
 
-    if (strcmp(pass1, pass2)) error(2, "Passwords don't match\n");
+    if (strcmp(pass1, pass2)) {free(fname); reimu_cancel(2, "Passwords don't match\n");}
 
     int rv;
-    if ((rv = rfbEncryptAndStorePasswd(pass1, fname)) != 0) error(rv, "Can't save password to %s (error %d)\n", fname, rv);
+    if ((rv = rfbEncryptAndStorePasswd(pass1, fname)) != 0) {free(fname); reimu_cancel(rv, "Can't save password to %s (error %d)\n", fname, rv);}
     free(fname);
     return 0;
 }
--- meta-mcst/meta-common/recipes-mcst/vncpasswd/vncpasswd_0.1.bb
+++ meta-mcst/meta-common/recipes-mcst/vncpasswd/vncpasswd_0.1.bb
@@ -5,7 +5,7 @@ PR = "r1"
 LICENSE = "GPLv2"
 LIC_FILES_CHKSUM = "file://LICENSE;md5=b234ee4d69f5fce4486a80fdaf4a4263"
 
-DEPENDS = "libvncserver"
+DEPENDS = "libvncserver libreimu"
 
 SRC_URI = " \
             file://LICENSE \
